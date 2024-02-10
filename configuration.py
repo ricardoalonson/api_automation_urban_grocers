d279dadd27c2df3d9bcfdae6cf9017786f5c5d98
ricardoalonson <ralonson1098@gmail.com>
2024-02-10 15:35:08 -0600

    Proyecto 06, Ricardo Alonso

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 # ---- PATHS ----
-URL_SERVICE = "https://cnt-287514ca-3762-4af5-b0e7-a88e5e9aede6.containerhub.tripleten-services.com"    # URL del servidor principal
+URL_SERVICE = "https://cnt-f03f846a-93d4-40da-8c50-c50e28d9e92c.containerhub.tripleten-services.com"    # URL del servidor principal
 CREATE_USER_PATH = "/api/v1/users/"# Ruta para crear un usuario o usuaria
 KITS_PATH = "/api/v1/kits/"# Ruta para crear un kit en esta variable
