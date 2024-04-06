736d8a1194550afe2a2eef3e5e6b18aaa0ba81eb
Ricardo Alonso <159092677+ricardoalonson@users.noreply.github.com>
2024-04-06 15:52:23 -0600

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 # ---- PATHS ----
-URL_SERVICE = "https://cnt-1479ec06-cf96-4a6c-bb57-68042616248c.containerhub.tripleten-services.com"    # URL del servidor principal
-CREATE_USER_PATH = "/api/v1/users/"# Ruta para crear un usuario o usuaria
-KITS_PATH = "/api/v1/kits/"# Ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-1479ec06-cf96-4a6c-bb57-68042616248c.containerhub.tripleten-services.com"    # main server URL
+CREATE_USER_PATH = "/api/v1/users/"  # Path (endpoint) to create a new user
+KITS_PATH = "/api/v1/kits/"  # Endpoint to create a kit 
